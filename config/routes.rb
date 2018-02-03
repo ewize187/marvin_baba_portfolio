a2f684e11529462cda3d22e21083b1b752828bed
Marvin Baba <afreecabiz@gmail.com>
2018-02-03 12:38:09 -0600

    Updates routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/Home'
-
-  get 'pages/About'
-
-  get 'pages/Contact'
-
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'About-Me', to: 'pages#About'
+  get 'Contact', to: 'pages#Contact'
+
+
+
+  root to: 'pages#Home'
 end
